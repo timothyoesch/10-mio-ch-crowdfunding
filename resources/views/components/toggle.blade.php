9f5035c7e8ec834db0f6040fc67a1dbd32792444
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-09-20 01:58:29 +0200

    Adjust font hierarchy in toggles on mobile

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -3,7 +3,7 @@
         <span>{{ $title }}</span>
         <x-heroicon-s-chevron-down class="h-6 md:h-8 mr-2 shrink-0"/>
     </p>
-    <div class="fcksvp-toggle__content--container max-h-0 overflow-hidden text-2xl">
+    <div class="fcksvp-toggle__content--container max-h-0 overflow-hidden text-lg md:text-xl">
         <div class="fcksvp-toggle__content pt-2 pb-6 opacity-0 translate-y-4">
             {!! $slot !!}
         </div>
