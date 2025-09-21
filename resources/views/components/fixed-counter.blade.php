6b62e06c4d5e8c49685fe2d0b35f21cc3ae3f174
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-09-21 20:48:57 +0200

    Enlarge padding for fixed counter on mobile

--- resources/views/components/fixed-counter.blade.php
+++ resources/views/components/fixed-counter.blade.php
@@ -1,4 +1,4 @@
-<div class="fcksvp-fixedcounter fixed z-20 bottom-0 left-0 right-0 bg-dark p-3">
+<div class="fcksvp-fixedcounter fixed z-20 bottom-0 left-0 right-0 bg-dark py-5 md:py-3">
     <div class="flex justify-center items-baseline gap-x-1 text-white text-sm">
         <span>{{__("landing.counter.fixed.live")}}</span>
         <p class="leading-[0.9] font-black font-mono" id="fixed-amountcounter">
