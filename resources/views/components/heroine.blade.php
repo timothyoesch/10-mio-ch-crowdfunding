d42a9abea35b940877b008f1d409e42c4ad5ec47
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-09-22 15:38:53 +0200

    Add larger sizes of visual

--- resources/views/components/heroine.blade.php
+++ resources/views/components/heroine.blade.php
@@ -3,13 +3,13 @@
         <picture>
         <source
             type="image/avif"
-            srcset="images/visual-3-100.avif?width=100 100w, images/visual-3-200.avif?width=200 200w, images/visual-3-400.avif?width=400 400w, images/visual-3-800.avif?width=800 800w" />
+            srcset="images/visual-3-100.avif?width=100 100w, images/visual-3-200.avif?width=200 200w, images/visual-3-400.avif?width=400 400w, images/visual-3-800.avif?width=800 800w, images/visual-3-1600.avif?width=1600 1600w" />
         <source
             type="image/webp"
-            srcset="images/visual-3-100.webp?width=100 100w, images/visual-3-200.webp?width=200 200w, images/visual-3-400.webp?width=400 400w, images/visual-3-800.webp?width=800 800w" />
+            srcset="images/visual-3-100.webp?width=100 100w, images/visual-3-200.webp?width=200 200w, images/visual-3-400.webp?width=400 400w, images/visual-3-800.webp?width=800 800w, images/visual-3-1600.webp?width=1600 1600w" />
         <img
             src="images/visual-3.png"
-            srcset="images/visual-3-100.png?width=100 100w, images/visual-3-200.png?width=200 200w, images/visual-3-400.png?width=400 400w, images/visual-3-800.png?width=800 800w"
+            srcset="images/visual-3-100.png?width=100 100w, images/visual-3-200.png?width=200 200w, images/visual-3-400.png?width=400 400w, images/visual-3-800.png?width=800 800w, images/visual-3-1600.png?width=1600 1600w"
             sizes="(max-width: 800px) 100vw, 50vw"
             style="width: 100%; aspect-ratio: 2.6666666666666665"
             loading="lazy"
